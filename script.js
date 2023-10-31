904d352aa994b15e2695655ba518ff240334a642
victorvang45 <victorvang456@gmail.com>
2023-10-30 19:45:24 -0500

    Created Calculator layout and added css

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function plusminus(num1) {
+    return num1 * -1 ;
+}
+
+function percent(num1) {
+    return num1 / 100; 
+}
